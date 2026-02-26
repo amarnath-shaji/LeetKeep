46d63e254a0eab13d7d79ed08c1692225be05cab
amarnath <amarnathshaji22@gmail.com>
2026-02-26 15:12:05 +0530

    Time: 4 ms (18.45%), Space: 54.4 MB (80.34%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+ 
+var intersection = function(nums1, nums2) {
+    
+var result = [];
+    for(let i = 0 ; i <= nums1.length ; i++){
+            if(nums2.includes(nums1[i])){
+                result.push(nums1[i])  
+        }
+    }
+    
+    let output = [...new Set(result)];
+    return output;
+};
+
+
+
