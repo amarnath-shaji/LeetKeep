9a70c7a24fad420b6c9b4ac398aff4b5770df098
amarnath <amarnathshaji22@gmail.com>
2026-04-10 14:40:43 +0530

    Time: 0 ms (100%), Space: 58 MB (53.57%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,20 +3,20 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    let rs = [];
-
-
-      for(let i= 1; i<=n; i++){
-        if(i%3===0 && i%5===0){
-            rs.push("FizzBuzz")
-        }else if(i%3===0){
-            rs.push("Fizz")
-        }else if(i%5===0){
-            rs.push("Buzz")
-        }else {
-            rs.push(String(i))    
+    let arr=[]
+    for(let i=1;i<=n;i++){
+        if(i % 3 ==0 && i % 5 ==0){
+            arr.push("FizzBuzz")
         }
-      }
-      return rs;
+        else if(i % 3 ===0){
+            arr.push("Fizz");
+        }else if(i % 5 ===0){
+            arr.push("Buzz")
+        }else{
+            arr.push(String(i))
+        }
+        
+    }
+    return arr
 };
 
